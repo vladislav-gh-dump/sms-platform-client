9f7f8428e8f9bb02c97839318af739a058dbefa6
vladislav-dump <vladislav.notebook@gmail.com>
2025-03-04 18:31:18 +0300

    Add parsing args and sending sms

--- sms_cli/__main__.py
+++ sms_cli/__main__.py
@@ -1,5 +1,16 @@
+import argparse
+from .send_sms import send_sms
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sender", help="Sender's phone number")
+    parser.add_argument("recipient", help="Recipient's phone number")
+    parser.add_argument("message", help="Message text")
     
+    args = parser.parse_args()
+    send_sms(args.sender, args.recipient, args.message)
+
+
 if __name__ == "__main__":
     main()
