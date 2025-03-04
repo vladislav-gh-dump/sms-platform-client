5784b0745b26b780828427d576a391efa17c58af
vladislav-dump <vladislav.notebook@gmail.com>
2025-03-04 17:31:55 +0300

    Refactoring: rename sms-cli package to sms_cli

--- sms-cli/utils.py
+++
@@ -1,8 +0,0 @@
-import tomllib
-
-
-def load_toml(file_path: str) -> dict:
-    """Загрузка данных из TOML файла."""
-    
-    with open(file_path, 'rb') as toml_file:
-        return tomllib.load(toml_file)
