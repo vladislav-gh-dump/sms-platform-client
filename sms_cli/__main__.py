6b467258d4e545d9d0aba33cde0028263fd0575d
Владислав <137087066+vladislav-gh-dump@users.noreply.github.com>
2025-03-04 23:29:12 +0300

    Fix argparse handling

--- sms_cli/__main__.py
+++ sms_cli/__main__.py
@@ -10,14 +10,18 @@ def main():
     parser.add_argument("recipient", help="Recipient's phone number")
     parser.add_argument("message", help="Message text")
     
+    if len(sys.argv) == 2 and sys.argv[1] in ("-h", "--help"):
+        parser.parse_args()
+        sys.exit(0)
+    
     if len(sys.argv) != 4:
         logger.error("Not enough arguments provided")
         print("Argument Error: Not enough arguments provided")
         sys.exit(1)
-    
+
     args = parser.parse_args()
     logger.info(f"Args:\nSender: {args.sender}\nRecipient: {args.recipient}\nMessage: {args.message}")
-    
+
     send_sms(args.sender, args.recipient, args.message)
 
 
